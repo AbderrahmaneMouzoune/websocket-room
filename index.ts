07a237ec5ee89adea1fd8b749deeb113d6599bb6
AbderrahmaneMouzoune <abderrahmane.mouzoune@gmail.com>
2024-06-15 06:39:37 +0200

    feat: message to room

--- index.ts
+++ index.ts
@@ -4,6 +4,12 @@ import { fileURLToPath } from "node:url"
 import { dirname, join } from "node:path"
 import { Server } from "socket.io"
 
+type TRoom = {
+  id: string
+  host: {
+    username: string
+  }
+}
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
@@ -15,13 +21,18 @@ app.get("/", (req, res) => {
 })
 
 io.on("connection", (socket) => {
-  console.log("a user connected")
+  const room: TRoom = JSON.parse(socket.handshake.query["room"] as string)
+  const user: string = socket.handshake.query["user"] as string
+  console.log(`${user} connected to ${room.id}`)
+
+  socket.join(room.id)
+
   socket.on("disconnect", () => {
     console.log("user disconnected")
   })
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg)
+  socket.on("chat message", (msg: string) => {
+    io.to(room.id).emit(`chat message`, `${msg}, from ${user}`)
     console.log("message: " + msg)
   })
 })
